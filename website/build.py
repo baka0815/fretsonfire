2ad0d7ab01fa1623cf93a21437c3447a01924444
skyostil <skyostil@a57bf1a8-ed29-0410-8531-e1fc4085a052>
2007-12-07 13:30:09 +0000

    Added website sources.

---
+++ website/build.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import os, sys
+# We add the Plugins path to the current Python path
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), "Plugins"))
+try:
+	from tahchee.main import *
+except:
+	print "Unable to import Tahchee."
+	print "Please check that the 'tahchee' module is in your PYTHONPATH"
+	sys.exit(-1)
+# You can change the following things
+# =============================================================================
+URL       = "http://fretsonfire.sourceforge.net"
+MAIN      = "index.html"
+SHOW_MAIN = True
+INDEXES   = ["index.*"]
+IGNORES   = ["*.sw?", "*.bak", "*.pyc", ".cvs", ".CVS", ".svn", ".svn/*", ".DS_Store", "*.*~"]
+TIDYFLAGS = "-i"
+ACCEPTS   = []
+# =============================================================================
+# Do not modify this code
+if __name__ == "__main__":
+	print "tahchee v." + version()
+	site = Site(URL, locals=locals())
+	if len(sys.argv)>1 and sys.argv[1].lower()=="remote": site.setMode("remote")
+	SiteBuilder(site).build(filter(lambda x:x not in ('local','remote'),sys.argv[1:]))
